b7fccf0420328726d6a2143539ab6109db5667ad
Danny Chou <danchouzhou@gmail.com>
2022-12-16 18:36:18 +0800

    Update ports

--- circuitpy/pin.py
+++ circuitpy/pin.py
@@ -5,17 +5,21 @@ Pin = {
     "D12" : board.D12,
     "D11" : board.D11,
     "D10" : board.D10,
-    "D9" : board.D9,
-    "D6" : board.D6,
-    "D5" : board.D5,
+    "D9"  : board.D9,
+    "D6"  : board.D6,
+    "D5"  : board.D5,
+    "SCL" : board.SCL,
+    "SDA" : board.SDA,
+    "D4"  : board.NEOPIXEL,
     "NEOPIXEL" : board.NEOPIXEL,
-    "A5" : board.A5,
-    "A4" : board.A4,
-    "A3" : board.A3,
-    "A2" : board.A2,
-    "A1" : board.A1,
-    "A0" : board.A0
+    "A5"  : board.A5,
+    "A4"  : board.A4,
+    "A3"  : board.A3,
+    "A2"  : board.A2,
+    "A1"  : board.A1,
+    "A0"  : board.A0
 }
 
 def pin(p):
     return Pin[p]
+
